7bdac2cffcfb0a7e565144090758504e4eaf1323
FREDDY ANDREY GONZALEZ ARTAVIA <andreyga22@gmail.com>
2024-03-21 21:07:11 -0600

    Added app background

--- P6Assets_FreddyG/MauiProgram.cs
+++ P6Assets_FreddyG/MauiProgram.cs
@@ -1,20 +1,21 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
-namespace P6Assets_FreddyG {
-    public static class MauiProgram {
-        public static MauiApp CreateMauiApp() {
+namespace P6Assets_FreddyG
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
             var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts => {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
-
+            builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            }).UseMauiCommunityToolkit();
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
-
             return builder.Build();
         }
     }
